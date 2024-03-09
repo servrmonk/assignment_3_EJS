422549f21e4170eb8f66b75915e1831126181297
Raushan <roushankumarsingh2018@gmail.com>
2024-03-09 23:37:30 +0530

    removing comments

--- routes/admin.js
+++ routes/admin.js
@@ -4,22 +4,15 @@ const express = require("express");
 
 const rootDir = require("../util/path");
 
-// import product controller
-const productsController = require("../controllers/products"); //productsController bundles all the exported functions
+
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-// const products = []; cut it out for product.js
 
-// /admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct); //productsController.getAddProduct() we don't execute this function so don't add () paranthesis instead of that we just pass the reference of the function . we r just telling express through express router that it should productsController.getAddProduct take this functionand store it and whenever the request reaches the routes /add-product it should go ahead and executes. and now as i do we can repeat this for below code also
-
-// /admin/add-product => POST
+router.get("/add-product", productsController.getAddProduct); 
 router.post("/add-product", productsController.postAddProduct);
-// and twik this export also we don't have to export products longer because it is inside product.js
-// exports.routes = router;
-// exports.products = products;
 
 module.exports = router;
 
-// admin controller and product controller
+
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,9 @@ const path = require("path");
 
 const express = require("express");
 
-// const rootDir = require("../util/path");
-// const adminData = require("./admin");
-
-// i can emport  product controller from product.js
-const productController = require('../controllers/products')
+const productController = require("../controllers/products");
 const router = express.Router();
-//the logic  here is typical controller logic
-router.get("/",productController.getProducts );
+
+router.get("/", productController.getProducts);
 
 module.exports = router;
