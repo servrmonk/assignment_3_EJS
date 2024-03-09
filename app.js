650bde6219386e911fcab197357e0038faf2cf45
Raushan <roushankumarsingh2018@gmail.com>
2024-03-09 23:55:53 +0530

    added 404 controller and updated app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,8 +18,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+// app.use((req, res, next) => {
+// for that there is rendering 404 file , creted new controllers error.js, and now i cut this middleware and add in error.js file 
+//   res.status(404).render("404", { pageTitle: "Page Not Found" });
+// });
+
+app.use(errorController.get404);//as a reference to the function
 
 app.listen(3000);
